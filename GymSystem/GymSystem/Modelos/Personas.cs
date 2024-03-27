2e27b780ad5e2fdb973303ec358fa433e0cc1ac9
MaxDanna26 <maximilianodanna@hotmail.com>
2024-03-27 20:32:59 +0100

    MakingWinForms

---
+++ GymSystem/GymSystem/Modelos/Personas.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymSystem.Modelos
+{
+    public class Personas
+    {
+        public string nombre;
+        public string apellido;
+        public string tel;
+
+        
+      
+    }
+}
